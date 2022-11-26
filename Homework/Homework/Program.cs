35623e1d1ba9715b7da4d8fb8640f3be63053b57
ZumaMamedova <zumrudam@code.edu.az>
2022-11-26 13:26:57 +0400

    Split

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name;
+        string surname;
+        string fullname = "Lorem Ipsum";
+
+        string[] arr = fullname.Split(' ');
+        {
+            if (!String.IsNullOrEmpty(arr[0]))
+            {
+                name = arr[0];
+                Console.WriteLine("name is" + arr[0]);
+            }
+            if (!String.IsNullOrEmpty(arr[1]))
+                surname = arr[1];
+            Console.WriteLine("surname is" +  arr[1]);
+        }
+
+    }
+}
