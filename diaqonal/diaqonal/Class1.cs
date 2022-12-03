3f90dc08ab930276e8c2bc045e567c67a701e78f
ZumaMamedova <zumrudam@code.edu.az>
2022-12-03 23:56:34 +0400

    zxc

---
+++ diaqonal/diaqonal/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diaqonal
+{
+    internal class Class1
+    {
+        public void Asdf(int[,] a)
+        {
+            int sum1 = 0;
+            int sum2 = 0;
+            int j = a.GetLength(1) - 1;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                sum1 = sum1 + a[i, i];
+            }
+            for (int i = 0; i < a.GetLength(1)-1; i++)
+            {
+                sum2 = sum2 + a[j, j];
+                j--;
+            }
+            Console.WriteLine(sum1 - sum2);
+        }
+
+    }
+}
---
+++ diaqonal/diaqonal/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using diaqonal;
+{
+    static void Main(string[] args)
+    {
+        int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        Class1 zumrud1 = new Class1();
+        zumrud1.Asdf(arr);
+
+    }
+}
